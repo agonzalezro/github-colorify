2f428b43ed85e22034ce4b63360da178086fc6a6
Álex González <agonzalezro@gmail.com>
2015-01-18 15:03:33 +0000

    Colors are grouped by project

---
+++ README.md
@@ -0,0 +1,31 @@
+github-colorify
+===============
+
+github-colorify is an extension for:
+
+- [x] Chrome
+- [ ] Firefox
+
+that will add colors to the rows on your github timeline. This will allow you
+to easily differentiate between projects because they will be grouped with the
+same background color.
+
+Installation
+------------
+
+This project is in really early stages. If you want to install it you will need
+to:
+
+1. go to [chrome://extensions](chrome://extensions)
+2. activate the developer mode.
+3. Click on "Load unpacked extension" and select the path were you have
+   downloaded this extension.
+
+TODO
+----
+- Add screenshot to the README.
+- Improve the CSS applied a little bit.
+- Persist the colors with some local storage.
+- Allow configuration for the "seen" projects background color.
+- Allow configuration for the list of colors.
+- Calculate the color instead set a random one?
--- colorify.js
+++ colorify.js
@@ -1,19 +1,29 @@
 if (document.title === "GitHub") {
-  COLORS = [
-    "fce94f", "edd400", "c4a000",
-    "fcaf3e", "f57900", "ce5c00",
-    "e9b96e", "c17d11", "8f5902",
-    "8ae234", "73d216", "4e9a06",
-    "729fcf", "3465a4", "204a87",
-    "ad7fa8", "75507b", "5c3566",
-    "ef2929", "cc0000", "a40000",
-    "eeeeec", "d3d7cf", "babdb6",
-    "888a85", "555753", "2e3436"
+  var colors = {}
+  colorify();
+}
+
+function colorify() {
+  BGCOLORS = [
+    "fce94f", "fcaf3e", "e9b96e", "8ae234", "729fcf", "ad7fa8", "ef2929", "eeeeec", "888a85"
   ]
 
   $(".alert").each(function(_, e) {
-    color = "#" + COLORS[Math.floor(Math.random() * COLORS.length)]
+    var project = getProjectName(e);
+    var color = getColorForProject(project)
+
     e.style.backgroundColor = color;
     e.style.paddingTop = "10px";
   });
+}
+
+function getProjectName(e) {
+  return $(e).find('.title > a:last').text().split('#')[0];
+}
+
+function getColorForProject(project) {
+  if (!(project in colors)) {
+    colors[project] = "#" + BGCOLORS[Math.floor(Math.random() * BGCOLORS.length)]
+  }
+  return colors[project];
 }
